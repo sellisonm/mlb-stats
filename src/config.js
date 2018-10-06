d047af55ad0731b448157d7562bb1d82bee32cfc
Scott Ellison <sellison@eVestment.com>
2018-10-06 13:35:53 -0400

    adding api call to MySportsFeeds along with sample config

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export default {
+	MySportsFeeds_APITOKEN: 'YOUR_API_KEY_GOES_HERE',
+	MySportsFeeds_PASSWORD: 'YOUR_PASSWORD_GOES_HERE',
+	MySportsFeeds_FORMAT: 'json'
+};
--- src/services/players.js
+++ src/services/players.js
@@ -1,11 +1,33 @@
 import axios from 'axios';
- 
-export default {
 
-    getPlayerData() {        
-        return axios.get('https://data.lacity.org/api/views/nxs9-385f/rows.json')
-        .then(function(response) {
-            return response;
-        })
-    }
-}
+import config from '../config';
+
+export default {
+	getPlayerData() {
+		var base64Token = btoa(
+			config.MySportsFeeds_APITOKEN + ':' + config.MySportsFeeds_PASSWORD
+		);
+		var authToken = 'Basic ' + base64Token;
+
+		var seasonName = '2018-playoff';
+		var gameIdentifier = '20181005-ATL-LAD';
+
+		var apiURL =
+			'https://api.mysportsfeeds.com/v1.1/pull/mlb/' +
+			seasonName +
+			'/game_boxscore.' +
+			config.MySportsFeeds_FORMAT +
+			'?gameid=' +
+			gameIdentifier;
+
+		return axios
+			.get(apiURL, {
+				headers: {
+					Authorization: authToken
+				}
+			})
+			.then(function(response) {
+				return response;
+			});
+	}
+};
