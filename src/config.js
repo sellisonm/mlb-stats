974671aff73010f5712aa047848b6b2a263e0b4e
Scott Ellison <sellison@eVestment.com>
2018-10-06 13:40:01 -0400

    test

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-	MySportsFeeds_APITOKEN: 'YOUR_API_KEY_GOES_HERE',
+	MySportsFeeds_APITOKEN: 'YOUR_API_KEY_GOES_HERE_x',
 	MySportsFeeds_PASSWORD: 'YOUR_PASSWORD_GOES_HERE',
 	MySportsFeeds_FORMAT: 'json'
 };
